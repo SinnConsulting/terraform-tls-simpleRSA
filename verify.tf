69d7a8f03a70258ce41855dd470c41f14caf00fd
ArgonQQ <marcel@sinn.consulting>
2021-05-02 02:08:32 +0200

    Fix old syntax

--- verify.tf
+++ verify.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "check_certs" {
-  count = "${length(local_file.server_cert.*.filename)}"
+  count = length(local_file.server_cert.*.filename)
   provisioner "local-exec" {
     command = "openssl verify -CAfile ${local_file.root_ca_cert.filename} -untrusted ${local_file.intermediate_ca_cert.filename} ${element(local_file.server_cert.*.filename, count.index)}"
   }
