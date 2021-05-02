7c1662a8f724810a5ea65a9a7c1f71e90c8b4bcb
ArgonQQ <marcel@sinn.consulting>
2021-05-02 02:00:03 +0200

    Implement cert check

--- verify.tf
+++ verify.tf
@@ -1,7 +1,6 @@
-# // use openssl to verify certs that have been generated.
-# resource "null_resource" "check_certs" {
-#   count = "${length(local_file.server_cert.*.filename)}"
-#   provisioner "local-exec" {
-#     command = "openssl verify -CAfile ${local_file.root_ca_cert.filename} -untrusted ${local_file.intermediate_ca_cert.filename} ${element(local_file.server_cert.*.filename, count.index)}"
-#   }
-# }
+resource "null_resource" "check_certs" {
+  count = "${length(local_file.server_cert.*.filename)}"
+  provisioner "local-exec" {
+    command = "openssl verify -CAfile ${local_file.root_ca_cert.filename} -untrusted ${local_file.intermediate_ca_cert.filename} ${element(local_file.server_cert.*.filename, count.index)}"
+  }
+}
